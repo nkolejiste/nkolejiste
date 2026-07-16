72ec9868c3588ee633554f364b4840a19363e496
nkolejiste <nkolejiste@gmail.com>
2026-07-16 21:51:44 +0200

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,49 +1,81 @@
 const locomotives = [
   {
-    id: "363-5",
-    name: "363.5",
+    id: "363-526",
+    name: "363.526",
     title: "Řada 363.5",
     type: "elektricka",
     typeLabel: "Elektrická lokomotiva",
     image: "assets/images/lokomotivy/363-5/363_5.jpg",
-    shortDescription: "Modernizovaná dvousystémová lokomotiva určená především pro nákladní dopravu.",
-    description: "Řada 363.5 představuje modernizovanou podobu československých dvousystémových lokomotiv. Spojuje tradiční koncepci s novější výzbrojí a v současném provozu je spojována především s nákladní dopravou.",
+    shortDescription: "Modernizovaná dvousystémová lokomotiva určená především pro traťovou nákladní dopravu.",
+    description: "Lokomotiva 363.526 v korporátním laku ČD Cargo slouží na modelovém kolejišti především k dopravování nákladních vlaků kategorie Pn a NEx mezi stanicemi Olomouc a Hranice.",
     stats: {
       "Druh pohonu": "Elektrický",
-      "Napájecí soustava": "Dvousystémová",
+      "Napájecí soustava": "3 kV + 25 kV",
+      "Provozovatel": "ČD Cargo",
+      "Určení": "Traťová nákladní doprava"
+    }
+  },
+  {
+    id: "750-163",
+    name: "750.163",
+    title: "Řada 750",
+    type: "dieselova",
+    typeLabel: "Motorová lokomotiva",
+    image: "assets/images/lokomotivy/750/hlavni.svg",
+    shortDescription: "Československá motorová lokomotiva známá charakteristickým tvarem čelních oken.",
+    description: "Lokomotiva 750.163 v retro laku ČD Cargo slouží na modelovém kolejišti především jako 1. záloha ve stanici Olomouc. Její hlavní náplní práce je rozřazování vozů a sestavování vlaků.",
+    stats: {
+      "Druh pohonu": "Dieselelektrický",
+      "Spalovací motor": "K12V230DR",
       "Provozovatel": "ČD Cargo",
       "Určení": "Nákladní doprava"
     }
   },
   {
-    id: "750",
-    name: "750",
+    id: "750-333",
+    name: "750.333",
     title: "Řada 750",
     type: "dieselova",
-    typeLabel: "Dieselová lokomotiva",
+    typeLabel: "Motorová lokomotiva",
     image: "assets/images/lokomotivy/750/hlavni.svg",
-    shortDescription: "Výrazná československá dieselová lokomotiva známá charakteristickým tvarem čelních oken.",
-    description: "Lokomotivy řady 750 patří mezi nejznámější československé motorové stroje. Jejich nezaměnitelný vzhled a dlouholeté nasazení v osobní dopravě z nich udělaly jednu z ikon domácí železnice.",
+    shortDescription: "Československá motorová lokomotiva známá charakteristickým tvarem čelních oken.",
+    description: "Lokomotiva 750.333 v retro laku ČD Cargo slouží na modelovém kolejišti především jako 1. záloha ve stanici Olomouc. Její hlavní náplní práce je rozřazování vozů a sestavování vlaků.",
     stats: {
-      "Druh pohonu": "Dieselový",
-      "Přenos výkonu": "Elektrický",
-      "Provozovatel": "ČSD / ČD",
-      "Určení": "Osobní doprava"
+      "Druh pohonu": "Dieselelektrický",
+      "Spalovací motor": "K12V230DR",
+      "Provozovatel": "ČD Cargo",
+      "Určení": "Nákladní doprava"
     }
   },
   {
-    id: "753-7",
-    name: "753.7",
+    id: "753-741",
+    name: "753.741",
     title: "Řada 753.7",
     type: "dieselova",
-    typeLabel: "Dieselová lokomotiva",
+    typeLabel: "Motorová lokomotiva",
     image: "assets/images/lokomotivy/753-7/hlavni.svg",
     shortDescription: "Modernizovaná nákladní lokomotiva vycházející z legendární řady 753.",
-    description: "Řada 753.7 navazuje na původní lokomotivy řady 753, ale využívá modernizovaný pohon a novou techniku. Je typická především pro nákladní dopravu a soukromé dopravce.",
+    description: "Lokomotiva 753.741 v drobně odlišném korporátním laku ČD Cargo slouží na modelovém kolejišti především jako 3. záloha ve stanici Hranice. Její hlavní náplní práce je posun ve stanici Hranice a přísun vozů na místní rozsáhlou vlečku.",
     stats: {
-      "Druh pohonu": "Dieselový",
-      "Přenos výkonu": "Elektrický",
-      "Provozovatel": "Nákladní dopravci",
+      "Druh pohonu": "Dieselelektrický",
+      "Spalovací motor": "Caterpillar 3512 B DITA",
+      "Provozovatel": "ČD Cargo",
+      "Určení": "Nákladní doprava"
+    }
+  },
+  {
+    id: "753-776",
+    name: "753.776",
+    title: "Řada 753.7",
+    type: "dieselova",
+    typeLabel: "Motorová lokomotiva",
+    image: "assets/images/lokomotivy/753-7/hlavni.svg",
+    shortDescription: "Modernizovaná nákladní lokomotiva vycházející z legendární řady 753.",
+    description: "Lokomotiva 753.776 v korporátním laku ČD Cargo vykonává na modelovém kolejišti především posun s vozy na rozsáhlé vlečce v obvodu žst. Hranice.",
+    stats: {
+      "Druh pohonu": "Dieselelektrický",
+      "Spalovací motor": "Caterpillar 3512 B DITA",
+      "Provozovatel": "ČD Cargo",
       "Určení": "Nákladní doprava"
     }
   }
